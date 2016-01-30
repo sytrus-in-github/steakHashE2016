874af1095d89caa54e970ad42e4c38d827d8e51a
kwea <kwea@kwea123>
2016-01-30 16:06:23 +0100

    as

--- src/TestRound/Point.java
+++ src/TestRound/Point.java
@@ -9,9 +9,9 @@ public class Point {
 	}
 	
 	public static double squareDistance(Point a, Point b){
-		return Math.sqrt(Math.pow(a.x-b.x, 2)+Math.pow(a.y-b.y, 2));
+		return Math.pow(a.x-b.x, 2)+Math.pow(a.y-b.y, 2);
 	}
 	public static double distance(Point a, Point b){
-		return Math.pow(a.x-b.x, 2)+Math.pow(a.y-b.y, 2);
+		return Math.sqrt(Math.pow(a.x-b.x, 2)+Math.pow(a.y-b.y, 2));
 	}
 }
