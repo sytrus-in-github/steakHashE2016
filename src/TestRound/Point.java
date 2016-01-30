4dcb30bd91ad8cb8c2db228212726609cae3e079
kwea <kwea@kwea123>
2016-01-30 15:14:17 +0100

    csc

---
+++ src/TestRound/Point.java
@@ -0,0 +1,13 @@
+package TestRound;
+
+public class Point {
+	int id,x,y;
+	public Point(int id, int x, int y){
+		this.id = id;
+		this.x = x;
+		this.y = y;
+	}
+	public static double squareDistance(Point a, Point b){
+		return Math.pow(a.x-b.x, 2)+Math.pow(a.y-b.y, 2);
+	}
+}
