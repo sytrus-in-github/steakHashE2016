3c8e6fc3fafdea5f457f9d9ddaf6a0d9417287e0
ruoqi.he <supercontactor@gmail.com>
2016-02-11 20:35:36 +0100

    Command

--- src/firstRound/Command.java
+++ src/firstRound/Command.java
@@ -16,10 +16,33 @@ public class Command {
 	public int itemNumber;
 	
 	public Command(CommandType type, int droneId, int targetId, int itemType, int itemNumber) {
-		
+		this.type = type;
+		this.droneId = droneId;
+		this.targetId = targetId;
+		this.itemType = itemType;
+		this.itemNumber = itemNumber;
 	}
 
 	public void execute() {
 		
+	}
+	
+	@Override
+	public String toString() {
+		String result = droneId + " ";
+		if (type == CommandType.Load) {
+			result += "L ";
+		} else if (type == CommandType.Unload) {
+			result += "U ";
+		} else if (type == CommandType.Deliver) {
+			result += "D ";
+		} else if (type == CommandType.Wait) {
+			result += "W ";
+		}
+		result += targetId + " ";
+		if (type != CommandType.Wait) {
+			result += itemType + " " + itemNumber;
+		}
+		return result;
 	}
 }
