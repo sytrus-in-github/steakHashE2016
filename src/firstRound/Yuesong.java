74d776bcafca154a1a319fa2783d8212295d193f
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-02-11 21:28:35 +0100

    syncro

--- src/firstRound/Yuesong.java
+++ src/firstRound/Yuesong.java
@@ -13,6 +13,21 @@ public class Yuesong {
 		return false;
 	}
 	
+	public int closestOrder(Drone dr){
+		int id = -1,distmin = Q.nCol+Q.nRow,d;
+		for(int i = 0;i<Q.C;i++){
+			Order o = Q.orders[i];
+			if(o.totalItem>0){
+				d = Chiaman.turn(dr.x,dr.y,o.x,o.y);
+				if(d<distmin){
+					distmin = d;
+					id = i;
+				}
+			}
+		}
+		return id;
+	}
+	
 	public int findNearestUsefulWarehouse(Order o){
 		int min = -1,distmin = Q.nCol+Q.nRow,d;
 		for(int i = 0;i<Q.W;i++){
@@ -27,7 +42,12 @@ public class Yuesong {
 		return min;
 	}
 	
+	public int displace(Order o, Warehouse w){
+		return 0;
+	}
+	
 	public int alwaysNearest(Order i, ArrayList<Integer> warehouses){
+		Order o = null;
 		return 0;
 	}
 }
