9f72989a0434b92ad57b3eeb2523632929809785
ascane <mandy00724@gmail.com>
2016-02-11 20:48:31 +0100

    chiaman

--- src/firstRound/Chiaman.java
+++ src/firstRound/Chiaman.java
@@ -1,12 +1,22 @@
 package firstRound;
 
 public class Chiaman {
+	public static double dist2(double x1, double y1, double x2, double y2) {
+		return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+	}
 
-	public int nearestWarehouseEnough(int x, int y, int pType, int nItems) {
+	public static int nearestWarehouseEnough(int x, int y, int pType, int nItems) {
+		int id = -1;
+		double maxDist2 = Double.MAX_VALUE;
 		for (int i = 0; i < Q.W; i++) {
-//			if (Q.warehouses[i].products[pType] >= nItems && ) {
-				
+			if (Q.warehouses[i].products[pType] >= nItems) {
+				double tempDist2 = dist2(x, y, Q.warehouses[i].x, Q.warehouses[i].y);
+				if (tempDist2 < maxDist2) {
+					id = i;
+					maxDist2 = tempDist2;
+				}
 			}
 		}
+		return id;
 	}
 }
