9cd8304b48ba3d54a5a33757bfc126d6ca545ac7
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-02-11 21:09:22 +0100

    syncro

--- src/firstRound/Yuesong.java
+++ src/firstRound/Yuesong.java
@@ -1,5 +1,19 @@
 package firstRound;
 
-public class Yuesong {
+import java.util.ArrayList;
 
+public class Yuesong {
+	
+	public boolean isUseful(Order o, Warehouse w){
+		for(int i:o.items.keySet()){
+			
+		}
+	}
+	public int findNearestUsefulWarehouse(Order o){
+		
+	}
+	
+	public int alwaysNearest(Order i, ArrayList<Integer> warehouses){
+		
+	}
 }
