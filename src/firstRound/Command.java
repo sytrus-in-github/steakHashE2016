ecec0a3e735711189c714d4da253a9522d7abd49
kwea <kwea@kwea123>
2016-02-11 19:35:13 +0100

    df

--- src/firstRound/Command.java
+++ src/firstRound/Command.java
@@ -6,10 +6,22 @@ public class Command {
 	int x,y;
 	HashMap<Integer, Integer> items; //to a type, associate the number needed
 	
-	Command(String input){
-		String[] s = input.split(" ");
+	Command(String coordinate){
+		String[] s = coordinate.split(" ");
 		x = Integer.parseInt(s[0]);
 		y = Integer.parseInt(s[1]);
 		items = new HashMap<Integer, Integer>();
 	}
+	
+	void addItems(String input){
+		String[] s = input.split(" ");
+		for(String str : s){
+			if(!items.containsKey(Integer.parseInt(str)))
+				items.put(Integer.parseInt(str), 1);
+			else
+				items.put(Integer.parseInt(str), items.get(Integer.parseInt(str))+1);
+		}
+	}
+	
+	
 }
