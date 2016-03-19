1dc3c6449853bdfe06790549ffd3646f0f836015
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-03-19 11:57:24 +0100

    upload

---
+++ src/Coord.java
@@ -0,0 +1,21 @@
+
+public class Coord {
+	public static final int LATMIN = -324000, LATMAX = 324000,
+							LONMIN = -648000, LONMAX = 647999;
+	public int lat,lon;
+	
+	public Coord(int la,int lo){
+		lat = la;
+		lon = lo;
+	}
+	
+	Coord add(int la, int lo){
+		la %= 648000;
+		lat += la;
+		
+	}
+	
+	Coord add(Coord c){
+		return add(c.lat, c.lon);
+	}
+}
