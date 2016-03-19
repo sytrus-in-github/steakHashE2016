b60a876a09293e1b517743ed5364e0f861755e3e
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-03-19 12:40:07 +0100

    upload

--- src/finalRound/Album.java
+++ src/finalRound/Album.java
@@ -1,6 +1,7 @@
 package finalRound;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Album {
 	public int value;
@@ -8,7 +9,7 @@ public class Album {
 	public int nbInterval;
 	public ArrayList<Photo> photos;
 	public ArrayList<Pair> intervals;
-	public boolean[] taken;
+	public HashMap<Photo,Boolean> taken;
 	
 	public Album(int value, int nbfoto, int nbrange){
 		this.value = value;
@@ -33,6 +34,19 @@ public class Album {
 	public Photo addPhoto(int lat,int lon){
 		Photo ret = new Photo(lat, lon, this);
 		photos.add(ret);
+		taken.put(ret, false);
 		return ret;
+	}
+	
+	public void take(Photo p){
+		if(!photos.contains(p))
+			System.out.println("photo does not belong to the album!");
+		taken.put(p, true);
+	}
+	
+	public boolean isTaken(Photo p){
+		if(!photos.contains(p))
+			System.out.println("photo does not belong to the album!");
+		return taken.get(p);
 	}
 }
