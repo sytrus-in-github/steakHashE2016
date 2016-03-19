09746b43d000cb78093500afe36b0f154901a7d8
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-03-19 10:15:51 +0100

    test3

--- src/finalRound/Yuesong.java
+++ src/finalRound/Yuesong.java
@@ -2,6 +2,7 @@ package finalRound;
 
 public class Yuesong {
 	public static void main(String[] args){
+		Chiaman.main(null);
 		System.out.println("2 euro, please.");
 		System.out.println("Where is Rocky?");
 	}
