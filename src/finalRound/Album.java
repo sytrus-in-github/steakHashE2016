fc3647219437a7886ededadbafca19f21c3cf0b2
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-03-19 12:27:32 +0100

    upload

--- src/finalRound/Album.java
+++ src/finalRound/Album.java
@@ -1,18 +1,28 @@
 package finalRound;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Album {
 	public int value;
 	public int nbPhoto;
 	public int nbInterval;
-	public Photo[] photos;
-	public Pair[] intervals;
+	public ArrayList<Photo> photos;
+	public ArrayList<Pair> intervals;
+	public boolean[] taken;
 	
 	public Album(int value, int nbfoto, int nbrange){
 		this.value = value;
 		this.nbPhoto = nbfoto;
 		this.nbInterval = nbrange;
-		this.photos = new Photo[nbPhoto];
-		this.intervals = new Pair[nbInterval];
+		this.photos = new ArrayList<>(nbPhoto);
+		this.intervals = new ArrayList<>(nbInterval);
+	}
+	
+	public Photo addPhoto(Coord c){
+		Photo ret = new Photo(c, this);
+		photos.add(ret);
+		return ret;
 	}
 	
 }
