859382fe86ba11c240762b57d1a34795891e7edf
SuperContact <supercontact@126.com>
2016-03-19 14:27:19 +0100

    BasicSolve

--- src/finalRound/Ruoqi.java
+++ src/finalRound/Ruoqi.java
@@ -1,8 +1,34 @@
 package finalRound;
 
+import java.util.PriorityQueue;
+
 public class Ruoqi {
 
-	public static void main(String[] args) {
-		
+	public static void Solve() {
+		PriorityQueue<Album> sortedAlbums = null;
+		for (Album a : sortedAlbums) {
+			boolean albumGood = true;
+			for (Photo p : a.photos) {
+				boolean photoGood = false;
+				nextPhoto:
+				for (Pair interval : a.intervals) {
+					for (int i = 0; i < Q.satellites.length; i++) {
+						if (Q.satellites[i].setPhotoIfAvailable(interval, p)) {
+							photoGood = true;
+							break nextPhoto;
+						}
+					}
+				}
+				if (!photoGood) {
+					albumGood = false;
+					break;
+				}
+			}
+			if (!albumGood) {
+				for (int i = 0; i < Q.satellites.length; i++) {
+					Q.satellites[i].removePhotos(a);
+				}
+			}
+		}
 	}
 }
