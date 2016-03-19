361c752e436891947185ea9249a5b3fa41e634b9
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-03-19 16:25:07 +0100

    upload

--- src/finalRound/Satellite.java
+++ src/finalRound/Satellite.java
@@ -6,7 +6,7 @@ import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-public class PhotoTaken {
+class PhotoTaken {
 	public Photo photo;
 	public int t;
 	public int dLat;
