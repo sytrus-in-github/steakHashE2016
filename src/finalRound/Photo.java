e76e6b9fa3d9481e93ea00994be559da6f186b41
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-03-19 12:05:34 +0100

    upload

--- src/finalRound/Photo.java
+++ src/finalRound/Photo.java
@@ -1,3 +1,17 @@
 package finalRound;
 
-
+public class Photo{
+	
+	public Album album;
+	public Coord coord;
+	
+	public Photo(Coord c, Album a){
+		this.coord = c;
+		this.album = a;
+	}
+	
+	public Photo(int lat, int lon, Album a){
+		this.coord = new Coord(lat, lon);
+		this.album = a;
+	}
+}
