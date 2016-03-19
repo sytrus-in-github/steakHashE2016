12831d7752f0abcf9f135111d9668c67b231d68b
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2016-03-19 12:16:36 +0100

    upload

--- src/finalRound/Album.java
+++ src/finalRound/Album.java
@@ -1,5 +1,18 @@
 package finalRound;
 
 public class Album {
-	public 
+	public int value;
+	public int nbPhoto;
+	public int nbInterval;
+	public Photo[] photos;
+	public Pair[] intervals;
+	
+	public Album(int value, int nbfoto, int nbrange){
+		this.value = value;
+		this.nbPhoto = nbfoto;
+		this.nbInterval = nbrange;
+		this.photos = new Photo[nbPhoto];
+		this.intervals = new Pair[nbInterval];
+	}
+	
 }
